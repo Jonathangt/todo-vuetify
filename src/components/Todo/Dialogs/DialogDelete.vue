87e03bcf8aee0cfed4b10a9fdfaed718d9793a10
Jonathan <jonathanguevara27@hotmail.com>
2022-10-11 12:18:39 -0500

    chg mensaje de eliminacion

--- src/components/Todo/Dialogs/DialogDelete.vue
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -4,7 +4,7 @@
             <v-card-title class="text-h5">
                 Delete task?
             </v-card-title>
-            <v-card-text>Are you sure you wanna delete this gosh darn motherflipping task?.</v-card-text>
+            <v-card-text>are you sure you want to delete the task?.</v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn color="black darken-1" text @click="$emit('close')">
